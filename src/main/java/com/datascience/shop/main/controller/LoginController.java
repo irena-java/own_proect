c957953491c10793a330b3f04928182ff0325d21
Irena Vakulenko <ivvvv@ukr.net>
2021-07-29 21:29:58 +0300

    копирую работающий вариант для возможного отката

--- src/main/java/com/datascience/shop/main/controller/LoginController.java
+++ src/main/java/com/datascience/shop/main/controller/LoginController.java
@@ -14,7 +14,7 @@ public class LoginController implements Controller{
         String userName = req.getParameter("userName");
         String password = req.getParameter("password");
         User user = userService.getByUserName(userName);
-
+//s=req.getParameter()
         if(user.getPassword().equals(password)) {
             req.setAttribute("user", user);
             return new ControllerResultDto("profile");
